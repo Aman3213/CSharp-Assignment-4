8f977360977b45750e138770a7ddeec018ce39a1
Aman3213 <amanuelliben3000@gmail.com>
2022-08-15 22:17:23 +0300

    Forms added, set and get
    Forms added, get and set

--- WindowsFormsApp1/WindowsFormsApp1/Class1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Class1.cs
@@ -8,5 +8,13 @@ namespace frmBankAccount
 {
     public partial class Class1
     {
+        public int Number { get; set; }
+        public String Date { get; set; }
+        public int Sku { get; set; }
+        public String ItemName { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public bool ISAVA { get; set; }
+        public String type { get; set; }
     }
 }
